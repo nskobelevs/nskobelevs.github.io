85608b552d8edd008879b9321de4dffba00f5da4
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2019-12-22 20:55:16 +0000

    test

---
+++ p5js/Testing/sketch.js
@@ -0,0 +1,26 @@
+let size;
+let topLeftCorner;
+let squareSize;
+
+let ph;
+let peh;
+let penh;
+
+let c;
+
+let ratio;
+
+function setup() {
+    size = floor(sort([windowHeight, windowWidth])[0] * 0.99);
+    createCanvas(windowWidth*0.99, windowHeight*0.99);
+}
+
+function draw() {
+    background(0);
+
+    textSize(32);
+    fill(255);
+    noStroke()
+    text(touches, 100, 100);
+}
+
